f9ca2cd3cffa628aeaa7716485e3373fa26dead2
Matiu-dev <matiw97@wp.pl>
2025-08-11 16:59:46 +0200

    ui and product details update v2

--- app/src/main/java/pl/matiu/pantrytrack/product/FirstFragmentViewModel.kt
+++ app/src/main/java/pl/matiu/pantrytrack/product/FirstFragmentViewModel.kt
@@ -11,13 +11,17 @@ import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
 import pl.matiu.pantrytrack.productDatabase.ProductRepository
+import pl.matiu.pantrytrack.productDatabase.productDetails.Energy
+import pl.matiu.pantrytrack.productDatabase.productDetails.ProductDetailsEntity
+import pl.matiu.pantrytrack.productDatabase.productDetails.ProductDetailsRepository
 import pl.matiu.pantrytrack.productDatabase.scannedProductPhoto.ProductScannedEntity
 import pl.matiu.pantrytrack.productDatabase.scannedProductPhoto.ProductScannedRepository
 import javax.inject.Inject
 
 @HiltViewModel
 class FirstFragmentViewModel @Inject constructor(private val productRepository: ProductRepository,
-    private val productScannedRepository: ProductScannedRepository): ViewModel() {
+    private val productScannedRepository: ProductScannedRepository,
+    private val productDetailsRepository: ProductDetailsRepository): ViewModel() {
 
     private var _productList = MutableStateFlow<List<Product>?>(emptyList())
     val productList = _productList.asStateFlow()
@@ -29,6 +33,47 @@ class FirstFragmentViewModel @Inject constructor(private val productRepository: 
         Log.d("products", "products view model init")
         addInitialProducts()
         addInitialScannedProducts()
+        createProductDetails()
+    }
+
+    private fun createProductDetails() {
+        viewModelScope.launch {
+            withContext(Dispatchers.IO) {
+                Log.d("product details", "adding")
+                val productDetailsHomogenizowany = ProductDetailsEntity(
+                    productDetailsId = 0,
+                    numberOfProtein = 7.2,
+                    numberOfFat = 6.3,
+                    numberOfCarbohydrate = 13.0,
+                    numberOfSalt = 0.2,
+                    energy = Energy(138, "kcal"),
+                )
+
+                val productDetailsSkyr = ProductDetailsEntity(
+                    productDetailsId = 1,
+                    numberOfProtein = 9.6,
+                    numberOfFat = 0.0,
+                    numberOfCarbohydrate = 10.0,
+                    numberOfSalt = 0.06,
+                    energy = Energy(78, "kcal"),
+                )
+
+                val productDetailsWiejski = ProductDetailsEntity(
+                    productDetailsId = 2,
+                    numberOfProtein = 11.0,
+                    numberOfFat = 5.0,
+                    numberOfCarbohydrate = 2.0,
+                    numberOfSalt = 0.7,
+                    energy = Energy(97, "kcal"),
+                )
+
+                productDetailsRepository.deleteAllData()
+                productDetailsRepository.addProductDetails(productDetailsHomogenizowany)
+                productDetailsRepository.addProductDetails(productDetailsSkyr)
+                productDetailsRepository.addProductDetails(productDetailsWiejski)
+
+            }
+        }
     }
 
 
